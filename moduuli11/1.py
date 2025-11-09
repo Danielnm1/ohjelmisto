1ba05be62341ec09de718c2f3bf893b796a17aa9
danielnm1 <Danielmelke1629@gmail.com>
2025-11-09 11:51:33 +0200

    moduuli 11

---
+++ moduuli11/1.py
@@ -0,0 +1,36 @@
+class Publication:
+    def __init__(self, name):
+        self.name = name
+
+    def print_information(self):
+        print(f"Publication name: {self.name}")
+
+
+class Book(Publication):
+    def __init__(self, name, author, page_count):
+        super().__init__(name)
+        self.author = author
+        self.page_count = page_count
+
+    def print_information(self):
+        print(f"Book name: {self.name}")
+        print(f"Author: {self.author}")
+        print(f"Page count: {self.page_count}")
+
+
+class Magazine(Publication):
+    def __init__(self, name, chief_editor):
+        super().__init__(name)
+        self.chief_editor = chief_editor
+
+    def print_information(self):
+        print(f"Magazine name: {self.name}")
+        print(f"Chief editor: {self.chief_editor}")
+
+
+
+donald_duck = Magazine("Donald Duck", "Aki Hyyppä")
+compartment_no6 = Book("Compartment No. 6", "Rosa Liksom", 192)
+
+donald_duck.print_information()
+compartment_no6.print_information()
---
+++ moduuli11/2.py
@@ -0,0 +1,47 @@
+class Car:
+    def __init__(self, license_plate, maximum_speed):
+        self.license_plate = license_plate
+        self.maximum_speed = maximum_speed
+        self.current_speed = 0
+        self.travelled_distance = 0
+
+    def accelerate(self, speed_change):
+        """Change the speed of the car by the given amount,
+        without exceeding maximum_speed or going below 0."""
+        self.current_speed += speed_change
+        if self.current_speed > self.maximum_speed:
+            self.current_speed = self.maximum_speed
+        elif self.current_speed < 0:
+            self.current_speed = 0
+
+    def drive(self, hours):
+        """Drive the car for the given number of hours."""
+        self.travelled_distance += self.current_speed * hours
+
+    def print_information(self):
+        print(f"Car registration number: {self.license_plate}")
+        print(f"Maximum speed: {self.maximum_speed} km/h")
+        print(f"Current speed: {self.current_speed} km/h")
+        print(f"Travelled distance: {self.travelled_distance} km")
+
+
+class ElectricCar(Car):
+    def __init__(self, license_plate, maximum_speed, battery_capacity):
+        super().__init__(license_plate, maximum_speed)
+        self.battery_capacity = battery_capacity
+
+    def print_information(self):
+        super().print_information()
+        print(f"Battery capacity: {self.battery_capacity} kWh")
+
+
+class GasolineCar(Car):
+    def __init__(self, license_plate, maximum_speed, tank_volume):
+        super().__init__(license_plate, maximum_speed)
+        self.tank_volume = tank_volume
+
+    def print_information(self):
+        super().print_information()
+        print(f"Tank volume: {self.tank_volume} liters")
+auto =ElectricCar("ABC-15", 180 , 52.5)
+auto.print_information()
